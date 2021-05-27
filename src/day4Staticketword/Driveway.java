88a23dfafc12f8691eb36de08ba106cd18ffc582
HassanYu <65440093+HassanYouseef@users.noreply.github.com>
2021-05-27 23:47:27 +0300

    Update comments

--- src/day4Staticketword/Driveway.java
+++ src/day4Staticketword/Driveway.java
@@ -2,20 +2,20 @@ package day4Staticketword;
 
 public class Driveway {
 
-    // Static Mothod
+    //Static method
     public static int driveWaySize;
 
-    //Non Static Method
+    //Non-static method
     public void setDriveWaySize(int driveWaySize){
         this.driveWaySize = driveWaySize;
 
     }
-    //Non Static Method
+    //Non-static method
     public int getDriveWaySize(){
         return  driveWaySize;
     }
-    //Static Method
-    //You can call stATIC method without creating an object
+    //Static method
+    //You can call static method without creating an object
     public static void  shapeOfDriveWay(){
         System.out.println("Shape of drive way is rectangular");
     }
